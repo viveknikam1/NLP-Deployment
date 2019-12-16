cca345f55f08908929da24709ab70bebffa5b025
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2019-12-16 23:10:11 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from flask import Flask,request,render_template
+import pickle
+
+app= Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('Main.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    model = pickle.load(open('model.pkl','rb'))
+    
+    text = request.form['Feedback']
+    
+    cv = pickle.load(open('cv.pkl','rb'))
+    
+    x = cv.transform([text])
+    
+    x = x.toarray()
+    
+    str1= str(model.predict(x))
+    if str1=='[1]':
+        return ('<h1>' + request.form['Name']+" liked the restaurant" + '</h1>')
+    else:
+        return('<h1>' + request.form['Name']+" Not liked the restaurant" + '</h1>')
+     
+    '''
+    return "Hi Welcome to nlp"
+    
+
+if __name__ =='__main__':
+    app.run()
