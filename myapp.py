0809f80ca9034a37e94ee9d0115d1ae4b55942fc
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2019-12-17 22:58:09 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,request,render_template
 import pickle
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 app= Flask(__name__)
 @app.route('/')
@@ -9,27 +12,20 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
-    model = pickle.load(open('model.pkl','rb'))
-    
-    text = "wow nice"  #request.form['Feedback']
-    
-    cv = pickle.load(open('cv.pkl','rb'))
-    
-    x = cv.transform([text])
-    
-    x = x.toarray()
-    '''
-    str1= str(model.predict(x))
-    
-    if str1=='[1]':
-        return ('<h1>' + request.form['Name']+" liked the restaurant" + '</h1>')
+    PS = PorterStemmer()
+    text = request.form['Feedback']
+    RF_model = pickle.load(open('RF_model.pkl','rb'))
+    CV_model = pickle.load(open('CV.pkl','rb'))
+    msg = re.sub('[^a-zA-Z]',' ',text).lower().split()
+    msg = [PS.stem(word) for word in msg if not word in stopwords.words('english')]
+    msg = ' '.join(msg)
+    X = CV_model.transform([msg]).toarray()
+    output = RF_model.predict(X)[0]
+    if output==1:
+        return request.form['Name'] + ' is Liked the restaurant'
     else:
-        return('<h1>' + request.form['Name']+" Not liked the restaurant" + '</h1>')
-     
-    '''
-    return "Hi Welcome to nlp "+request.form['Feedback']
+        return request.form['Name'] + ' is Didnt Liked the restaurant'
     
 
 if __name__ =='__main__':
-    app.run()
+    app.run(debug = True)
